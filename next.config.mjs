4e3d823afb0ee4b71ae4d07e37cd34f2c3251665
Mikołaj Dudzik <dudzmiko@gmail.com>
2024-09-06 15:48:58 +0200

    Deploy update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/dudzmiko.dev',
     output: 'export',
     reactStrictMode: true,
+    images: {
+      unoptimized: true, // Aby uniknąć problemów z optymalizacją obrazów
+    },
   }
   
   export default nextConfig
+  
