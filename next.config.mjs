6e204cf118474aeba437fbfdb0abe32b0e991c4b
Mikołaj Dudzik <dudzmiko@gmail.com>
2024-09-06 15:20:14 +0200

    Update GitHub actions

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
     basePath: "/dudzmiko.dev",
     output: "export",
     reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
-            },
-        ],
-    },
 }
 
 export default nextConfig
