daa921873e7a8c87e02f20d85af27e1a2e23f828
Mikołaj Dudzik <dudzmiko@gmail.com>
2024-09-06 16:19:25 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/dudzmiko.dev', // Ścieżka na GitHub Pages
     output: "export",
     images: {
       loader: "akamai",
